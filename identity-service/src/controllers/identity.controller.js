4e1f448720aa76d211874eb676ea62a6ac85bea0
iAmNsengi <iamnsengi@icloud.com>
2025-01-05 17:59:46 +0200

    ft: add user registration validation schema

---
+++ identity-service/src/controllers/identity.controller.js
@@ -0,0 +1,7 @@
+import logger from "../utils/logger.js";
+
+export const registerUser = async (req, res) => {
+  logger.info("Registration endpoint hit....");
+  try {
+  } catch (error) {}
+};
---
+++ identity-service/src/utils/validations.js
@@ -0,0 +1,11 @@
+import Joi, { string } from "joi";
+
+export const validateRegistration = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email().required(),
+    password: Joi().string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
