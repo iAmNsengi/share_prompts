015e0cf9c90c0a45180ce1e121a8a1f7e6a74a7c
iAmNsengi <iamnsengi@icloud.com>
2025-03-06 15:34:47 +0200

    ft: add ratelimiting

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -6,6 +6,8 @@ import helmet from "helmet";
 import { rateLimit } from "express-rate-limit";
 import { RedisStore } from "rate-limit-redis";
 
+import logger from "./utils/logger";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -17,3 +19,24 @@ app.use(cors());
 app.use(express.json());
 
 // rate limiting
+const rateLimit = rateLimit({
+  windowMs: 15 * 60 * 100,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`Sensitive endpoint rate limit exceeded for IP ${req.ip}`);
+    res.status(429).json({ success: false, message: "Too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use(rateLimit);
+
+app.use((req, res, next) => {
+  logger.info(`Received ${req.method} request to ${req.url}`);
+  logger.info(`Request body ${req.body} `);
+  next();
+});
